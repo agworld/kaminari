cbd9cfd5d23abf6102efd40d35aca49ddc36b5b3
Matt Powell <matt@soundendeavour.com>
2011-07-07 18:06:04 -0700

    patch to fix :page scope warnings all over the place

--- lib/kaminari/models/active_record_extension.rb
+++ lib/kaminari/models/active_record_extension.rb
@@ -6,17 +6,18 @@ module Kaminari
     included do
       def self.inherited(kls) #:nodoc:
         super
-
-        kls.class_eval do
-          include Kaminari::ConfigurationMethods
-
-          # Fetch the values at the specified page number
-          #   Model.page(5)
-          scope :page, Proc.new {|num|
-            limit(default_per_page).offset(default_per_page * ([num.to_i, 1].max - 1))
-          } do
-            include Kaminari::ActiveRecordRelationMethods
-            include Kaminari::PageScopeMethods
+        if kls.superclass == ActiveRecord::Base
+          kls.class_eval do
+            include Kaminari::ConfigurationMethods
+  
+            # Fetch the values at the specified page number
+            #   Model.page(5)
+            scope :page, Proc.new {|num|
+              limit(default_per_page).offset(default_per_page * ([num.to_i, 1].max - 1))
+            } do
+              include Kaminari::ActiveRecordRelationMethods
+              include Kaminari::PageScopeMethods
+            end
           end
         end
       end
